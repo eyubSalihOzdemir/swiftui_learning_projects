df17d0a779e5a76078c7b5a98d81cb79ac153ef8
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-04-09 17:30:38 +0300

    added fifteenth project

---
+++ Project 15 - AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Salih Özdemir on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
